029ba4cbd111f48bc1ad3cb36f22a035fdbc2e0d
Mukarram Ali <mukarramali623@gmail.com>
2023-03-24 22:15:25 +0500

    SetupFlutter

--- SettingUp/T1.dart
+++ SettingUp/T1.dart
@@ -1,3 +1,22 @@
-void main(){
-    print("Hello world");
+import 'dart:io';
+
+class Num {
+  int num = 10;
+}
+
+main() {
+  stdout.writeln('What is Name: ?');
+  String name = stdin.readLineSync()!;
+
+  print("Hello world $name");
+  var n = Num();
+  int number;
+  number = n?.num ?? 0;
+  print(number);
+  var num1;
+  print(num1 ??= 2);
+
+  int x = 101;
+  var res = x % 2 == 0 ? 'Even' : 'Odd';
+  print(res);
 }
---
+++ SettingUp/android/app/src/main/kotlin/com/example/hgfds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hgfds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
