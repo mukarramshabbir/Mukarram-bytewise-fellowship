e48e924ea2949696082bdbbbee4c56f7d3c8d641
Mukarram Ali <mukarramali623@gmail.com>
2023-04-27 23:13:13 +0500

    Week06

---
+++ Week06/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text('Lottie Animations'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Lottie.network(''),
+      ),
+    );
+
+}
+
